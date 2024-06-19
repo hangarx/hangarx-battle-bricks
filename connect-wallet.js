010c5079ef20d96f63ab6eee786597764f78054b
truongdnn <nhattruong2513@gmail.com>
2024-06-19 17:19:14 +0700

    save

--- connect-wallet.js
+++ connect-wallet.js
@@ -69,7 +69,7 @@ function setCookie(cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
-function getCookie(cname) {
+const getCookie = (cname) => {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
   let ca = decodedCookie.split(';');
